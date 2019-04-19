085d42a471dc39ed71827e4c6cb52e577858c9f4
Kevin <kevinp7@cs.washington.edu>
2019-04-19 13:55:49 -0700

    more puns

--- RemoveDuplicates.java
+++ RemoveDuplicates.java
@@ -3,19 +3,24 @@ import java.io.*;
 
 public class RemoveDuplicates {
 	public static void main(String[] args) throws FileNotFoundException {
-		Scanner input = new Scanner(new File(args[0]));
-		Set<String> seenNames = new HashSet<String>();
+		File file = new File(args[0]);
+		if (file.exists()) {
+			Scanner input = new Scanner(new File(args[0]));
+			Set<String> seenNames = new HashSet<String>();
 		
-		while (input.hasNextLine()) {
-			String line = input.nextLine();
-			String name = line.split(":", 2)[0];
-			if (seenNames.contains(name)) {
-				System.out.println("Duplicate: " + name);
-			} else {
-				seenNames.add(name);
+			while (input.hasNextLine()) {
+				String line = input.nextLine();
+				String name = line.split(":", 2)[0];
+				if (seenNames.contains(name)) {
+					System.out.println("Duplicate: " + name);
+				} else {
+					seenNames.add(name);
+				}
 			}
+		} else {
+			System.out.println("File not found.");
+			System.out.println("Usage: java RemoveDuplicates fileName.txt");
 		}
-		
 		
 	}
 }
